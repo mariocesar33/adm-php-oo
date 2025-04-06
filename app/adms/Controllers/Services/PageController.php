bd05a3a6264f3fc8b512efe6037c7925e152213e
mariocesar33 <mcgato33@hotmail.com>
2025-04-06 22:50:14 -0100

    Criar URL amigável com htaccess

--- app/adms/Controllers/Services/PageController.php
+++ app/adms/Controllers/Services/PageController.php
@@ -3,12 +3,26 @@
 namespace App\adms\Controllers\Services;
 
 /**
- * Recebe a URL e manipula
- * 
  * @author: Mário César <mcgato33@hotmail.com>
 */
 class PageController {
+  /** @var string $url Recebe a URL do .htaccess */
+  private string $url;
+
+  /**
+   * Recebe a URL e manipula
+   */
   public function __construct() {
-    echo "Carregar página.";
+    echo "Carregar página.<br><br>";
+
+    // Verificar se vem valor na variável url enviada pelo .htaccess
+    if(!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))) {
+      // Se sim, atribui o valor a variável url
+      $this->url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
+
+      echo "Acesar o endereço: " .$this->url . "<br><br>";
+    } else {
+      echo "Acesar a página principal.<br><br>";
+    }
   }    
 }
