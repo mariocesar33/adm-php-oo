5a06e5b07ecfecd34352db27b53f79e6249e53df
mariocesar33 <mcgato33@hotmail.com>
2025-04-07 09:11:05 -0100

    Manipular classe e parâmetro

--- app/adms/Controllers/Services/PageController.php
+++ app/adms/Controllers/Services/PageController.php
@@ -9,30 +9,39 @@ use App\adms\Helpers\ClearUrl;
  * 
  * @author Mário César <mcgato33@hotmail.com>
  */
-class PageController {
-  /** @var string $url Receber a URL do .htaccess */
-  private string $url;
-  
-  /**
-   * Recebe a URL do .htaccess
-   */
-  public function __construct() {
-    echo "Carregar página.<br><br>";
+class PageController
+{
+    /** @var string $url Receber a URL do .htaccess */
+    private string $url;
 
-    // Verificar se vem valor na variável url enviada pelo .htaccess
-    if(!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))){
+    /** @var array $urlArray */
+    private array $urlArray;
+    
+    /**
+     * Recebe a URL do .htaccess
+     */
+    public function __construct()
+    {
+        echo "Carregar página.<br><br>";
 
-      // Receber o valor da variável url enviada pelo .htaccess
-      $this->url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
+        // Verificar se vem valor na variável url enviada pelo .htaccess
+        if(!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))){
 
-      echo "Acessar o endereço: " . $this->url . "<br><br>";
+            // Receber o valor da variável url enviada pelo .htaccess
+            $this->url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
 
-      // Chamar a classe helper para limpar a URL
-      $this->url = ClearUrl::clearUrl($this->url);
-      var_dump($this->url);
+            echo "Acessar o endereço: " . $this->url . "<br><br>";
 
-    }else{
-      echo "Acessar a página principal.<br><br>";
+            // Chamar a classe helper para limpar a URL
+            $this->url = ClearUrl::clearUrl($this->url);
+            var_dump($this->url);
+
+            // Converter a string da URL em um array
+            $this->urlArray = explode('/', $this->url);
+            var_dump($this->urlArray);
+
+        }else{
+            echo "Acessar a página principal.<br><br>";
+        }
     }
-  }
 }
--- index.php
+++ index.php
@@ -5,5 +5,5 @@ use App\adms\Controllers\Services\PageController;
 // Carregar o Composer
 require './vendor/autoload.php';
 
-// Instanciar a class PageController, responsável por manipular a URL
+// Instanciar a classe PageController, responsável em tratar a URL
 $url = new PageController();
