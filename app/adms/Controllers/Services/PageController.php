eb06f154107816d7cb350613ebc46ebd1fe617c7
mariocesar33 <mcgato33@hotmail.com>
2025-04-07 09:20:22 -0100

    update

--- app/adms/Controllers/Services/PageController.php
+++ app/adms/Controllers/Services/PageController.php
@@ -2,27 +2,46 @@
 
 namespace App\adms\Controllers\Services;
 
+use App\adms\Helpers\ClearUrl;
+
 /**
- * @author: Mário César <mcgato33@hotmail.com>
-*/
-class PageController {
-  /** @var string $url Recebe a URL do .htaccess */
-  private string $url;
+ * Recebe a URL e manipula
+ * 
+ * @author Mário César <mcgato33@hotmail.com>
+ */
+class PageController
+{
+    /** @var string $url Receber a URL do .htaccess */
+    private string $url;
 
-  /**
-   * Recebe a URL e manipula
-   */
-  public function __construct() {
-    echo "Carregar página.<br><br>";
+    /** @var array $urlArray */
+    private array $urlArray;
+    
+    /**
+     * Recebe a URL do .htaccess
+     */
+    public function __construct()
+    {
+        echo "Carregar página.<br><br>";
 
-    // Verificar se vem valor na variável url enviada pelo .htaccess
-    if(!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))) {
-      // Se sim, atribui o valor a variável url
-      $this->url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
+        // Verificar se vem valor na variável url enviada pelo .htaccess
+        if(!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))){
 
-      echo "Acesar o endereço: " .$this->url . "<br><br>";
-    } else {
-      echo "Acesar a página principal.<br><br>";
+            // Receber o valor da variável url enviada pelo .htaccess
+            $this->url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
+
+            echo "Acessar o endereço: " . $this->url . "<br><br>";
+
+            // Chamar a classe helper para limpar a URL
+            $this->url = ClearUrl::clearUrl($this->url);
+            var_dump($this->url);
+
+            // Converter a string da URL em um array
+            $this->urlArray = explode('/', $this->url);
+            var_dump($this->urlArray);
+
+        }else{
+            echo "Acessar a página principal.<br><br>";
+        }
     }
-  }    
 }
