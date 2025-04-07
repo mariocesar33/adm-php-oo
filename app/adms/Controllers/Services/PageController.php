b8e64b6f0a4068b08a11a59fc8c2afc333d5ae88
mariocesar33 <mcgato33@hotmail.com>
2025-04-07 01:00:03 -0100

    Limpar URL amigável

--- app/adms/Controllers/Services/PageController.php
+++ app/adms/Controllers/Services/PageController.php
@@ -2,27 +2,37 @@
 
 namespace App\adms\Controllers\Services;
 
-/**
- * @author: Mário César <mcgato33@hotmail.com>
-*/
-class PageController {
-  /** @var string $url Recebe a URL do .htaccess */
-  private string $url;
+use App\adms\Helpers\ClearUrl;
 
+/**
+ * Recebe a URL e manipula
+ * 
+ * @author Mário César <mcgato33@hotmail.com>
+ */
+class PageController {
+  /** @var string $url Receber a URL do .htaccess */
+  private string $url;
+  
   /**
-   * Recebe a URL e manipula
+   * Recebe a URL do .htaccess
    */
   public function __construct() {
     echo "Carregar página.<br><br>";
 
     // Verificar se vem valor na variável url enviada pelo .htaccess
-    if(!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))) {
-      // Se sim, atribui o valor a variável url
+    if(!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))){
+
+      // Receber o valor da variável url enviada pelo .htaccess
       $this->url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
 
-      echo "Acesar o endereço: " .$this->url . "<br><br>";
-    } else {
-      echo "Acesar a página principal.<br><br>";
+      echo "Acessar o endereço: " . $this->url . "<br><br>";
+
+      // Chamar a classe helper para limpar a URL
+      $this->url = ClearUrl::clearUrl($this->url);
+      var_dump($this->url);
+
+    }else{
+      echo "Acessar a página principal.<br><br>";
     }
-  }    
+  }
 }
